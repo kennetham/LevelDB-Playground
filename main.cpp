bc2d4aedf00524736ba37db56930dc4e2af0e9af
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2015-09-26 15:24:28 +0800

    [v0.5] Added delete function
    - Remove key value from Database
    
    Signed-off-by: Kenneth Ham <kenneth.ham@comp.nus.edu.sg>

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
 	}
 
 	string compressed_info;
-	string uncompressed_info = "This is a text test";
+	string uncompressed_info = "The quick brown fox jumps over the lazy dog";
 	string output;
 
 	snappy::Compress(uncompressed_info.data(), uncompressed_info.size(), &output);
@@ -49,8 +49,27 @@ int main(int argc, char **argv) {
 
 	db->Put(write_options, key_name.str(), value_details.str());
 
+	ostringstream redundant_key;
+	redundant_key << &output;
+
+	ostringstream redundant_value;
+	redundant_value << compressed_info;
+
+	db->Put(write_options, redundant_key.str(), redundant_value.str());
+
 	// Iterate over each item in the database and print them
 	leveldb::Iterator *kv_iterator = db->NewIterator(leveldb::ReadOptions());
+
+	for (kv_iterator->SeekToFirst(); kv_iterator->Valid(); kv_iterator->Next()) {
+		cout << kv_iterator->key().ToString() << " : " << kv_iterator->value().ToString() << endl;
+	}
+
+	// Deletes a key value from database
+	db->Delete(write_options, redundant_key.str());
+	cout << "Deletes a key & value from DB\n";
+
+	// Updates iterator to get new DB values
+	kv_iterator = db->NewIterator(leveldb::ReadOptions());
 
 	for (kv_iterator->SeekToFirst(); kv_iterator->Valid(); kv_iterator->Next()) {
 		cout << kv_iterator->key().ToString() << " : " << kv_iterator->value().ToString() << endl;
